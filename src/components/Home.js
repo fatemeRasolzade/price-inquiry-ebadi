11b9fab585a54759d41b71235e3efbcbc33f2ff3
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2023-05-23 20:53:41 +0330

    version2

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import CustomInput from "./utils/CustomInput";
 import CustomDropDown from "./utils/CustomDropDown";
 import EbadiLogo from "../public/img/Ebadi.png";
 import { CompanyList, priceInquiry } from "../services/apis/Inquiry";
+import toast from "react-hot-toast";
 
 const initialForm = {
   model: "",
@@ -16,12 +17,6 @@ const Home = () => {
   const [Companies, setCompanies] = useState([]);
   const [Models, setModels] = useState([]);
   const [Price, setPrice] = useState([]);
-
-  const [ErrorMessage, setErrorMessage] = useState(" ");
-
-  useEffect(() => {
-    setErrorMessage(" ");
-  }, []);
 
   useEffect(() => {
     CompanyList(form.company?.id ? form.company?.id : 0).then((res) => setModels(res));
@@ -45,8 +40,8 @@ const Home = () => {
         console.log(res);
         setPrice(res);
       })
-      .catch((error) => {
-        setErrorMessage("مشکلی از سمت سرور رخ داده است");
+      .catch(() => {
+        toast.error("مشکلی از سمت سرور رخ داده است");
       });
   };
 
@@ -59,10 +54,9 @@ const Home = () => {
         <CustomDropDown value={form} setValue={setForm} name="company" label="کمپانی" list={Companies} searchOption variant="standard" clearField={() => setForm(initialForm)} />
         <CustomDropDown value={form} setValue={setForm} name="model" label="مدل" list={Models} searchOption variant="standard" />
         <CustomInput value={form} setValue={setForm} name="year" label="سال ساخت " variant="standard" maxLength={4} JustNumber />
-        <p className={`${ErrorMessage ? "opacity-100" : "opacity-0"} text-red-600 transition h-4 w-full`}>{ErrorMessage}</p>
         <CustomBtn parentClassName="w-full" disabled={ChkDisabled()} text="استعلام" OnClick={handleClick} className="main-btn w-full" />
-        <p className="text-right w-full">قیمت کارشناسی برای همکار: {Price[1]}</p>
-        <p className="text-right w-full">قیمت کارشناسی برای مصرف کننده: {Price[0]}</p>
+        <p className="text-right w-full text-xs">قیمت کارشناسی برای همکار: {Price[1]}</p>
+        <p className="text-right w-full text-xs">قیمت کارشناسی برای مصرف کننده: {Price[0]}</p>
       </form>
     </div>
   );
